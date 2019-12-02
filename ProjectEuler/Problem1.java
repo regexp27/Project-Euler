2ce2682ed15d525a42e2705d79e7ca224cf9a16c
regexp27 <48853131+regexp27@users.noreply.github.com>
2019-12-02 18:02:18 +0900

    Add files via upload

---
+++ ProjectEuler/Problem1.java
@@ -0,0 +1,14 @@
+package ProjectEuler;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        double sum = 0;
+        for(int i = 1; i < 1000; i++){
+            if( i % 3 == 0 || i % 5 ==0){
+                sum = sum + i;
+                }
+        }
+        System.out.println(sum);
+        //System.out.println(sum);
+        }
+    }
